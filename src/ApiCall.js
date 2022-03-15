c82c1d17d7a7b6539fe987b441c161d92cb88e86
Luca MARTINOLLI <martinolli.luca@gmail.com>
2022-03-15 15:33:32 +0100

    test?

--- src/ApiCall.js
+++ src/ApiCall.js
@@ -1,37 +1,38 @@
 
 import React from 'react';
 
-export function GetUser() {
-    fetch("https://anilistmikilior1v1.p.rapidapi.com/getUser", {
-        "method": "POST",
-        "headers": {
-            "content-type": "application/x-www-form-urlencoded",
-            "x-rapidapi-host": "Anilistmikilior1V1.p.rapidapi.com",
-            "x-rapidapi-key": "bd70cf8008msheaeff67f7f17b55p1c2999jsna8efb7960695"
-        },
-        "body": {
-            "accessToken": "T0y3cOAUQIamXzwq4NMi3TNICPRGVnStsmZvKrLB"
-        }
-    })
-    .then(response => {
-        console.log(response);
-    })
-    .catch(err => {
-        console.error(err);
-    });
-    //return await response.json();
-}
 
 class ApiCall extends React.Component {
     constructor(props) {
         super(props);
         this.state = { };
-      }
+    }
+
+    async GetUser() {
+        const response = await fetch("https://anilistmikilior1v1.p.rapidapi.com/getUser", {
+            "method": "POST",
+            "headers": {
+                "content-type": "application/x-www-form-urlencoded",
+                "x-rapidapi-host": "Anilistmikilior1V1.p.rapidapi.com",
+                "x-rapidapi-key": "bd70cf8008msheaeff67f7f17b55p1c2999jsna8efb7960695"
+            },
+            "body": {
+                "accessToken": "T0y3cOAUQIamXzwq4NMi3TNICPRGVnStsmZvKrLB"
+            }
+        })
+        .then(response => {
+            console.log(response);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+        return response.json()
+    }
 
     render() {
         return (
             <div className="App">
-                <GetUser/>
+                {this.GetUser}
             </div>
         );
     }
