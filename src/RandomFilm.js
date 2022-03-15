af7ba03e65b1ae2cf69a1748caad3bc1a8b1898d
Luca MARTINOLLI <martinolli.luca@gmail.com>
2022-03-15 18:43:04 +0100

    mamma mia ca marche

--- src/RandomFilm.js
+++ src/RandomFilm.js
@@ -1,36 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 
 function getRandom(min, max) {
-    return Math.random() * (max - min) + min;
+    return Math.trunc(Math.random() * (max - min) + min);
+}
+const setFavoris = (id, url) => {
+    localStorage.setItem(id, url)
 }
 
-const getFilm = async() => {
-    const id = getRandom(1,6000)
-    const url = "https://api.themoviedb.org/3/movie/"+ id +"?api_key=3dceec64c036d3bb5cc3708d099863fa";
-    const response = await fetch(url, {
-        headers : { 
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-         }});
-    const json = await response.json();
-    console.log(json);
+const Film = ({ data }) => {
+    console.log(data);
     return (
         <div>
-            <p>{json.original_title}</p>
-            <Button onClick={setFavoris(id,url)}>save</Button>
+            <h1>{data.original_title}</h1>
+            <p>Synopsys: {data.overview}</p>
         </div>
-    );
+    )
 }
 
-const setFavoris = (id,url) => {
-    localStorage.setItem(id,url)
-}
-
-function RandomFilm() {
+const RandomFilm = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [film, setFilm] = useState([]);
+    const id = getRandom(1, 1000)
+    const url = "https://api.themoviedb.org/3/movie/" + id + "?api_key=3dceec64c036d3bb5cc3708d099863fa";
+    
+    useEffect(() => {
+        const getFilm = () => {
+            if(isLoading){
+                fetch(url, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                    }
+                })
+                    .then((response) => response.json())
+                    .then((data) => {setIsLoading(false);setFilm(data);console.log(data)})
+                    .catch(error => console.log(error));
+            }
+        }
+        getFilm();
+    }, [url,isLoading]);
+    if (isLoading) {
+        return <div>Chargement...</div>;
+    }
     return (
-        <div className="App">
-            {getFilm()}
+        <div>
+            {film && <Film data={film} />}
+            <Button onClick={setFavoris(id, url)}>save</Button>
         </div>
     );
 }
